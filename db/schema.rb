8d7dc5f4171c749cdcfa609e22a6be8b5f015585
Akihiro Sada <sada@netlab.jp>
2017-04-26 20:28:04 +0900

    run "bundle exec rails generate paperclip user avatar".

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426112459) do
+ActiveRecord::Schema.define(version: 20170426112754) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
