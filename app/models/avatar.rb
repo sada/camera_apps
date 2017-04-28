67cd57946509f5a0977b9057b7e2857032cecd40
Akihiro Sada <sada@netlab.jp>
2017-04-28 20:14:12 +0900

    add paperclip model sample to Avatar.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,3 +1,6 @@
 class Avatar < ApplicationRecord
   belongs_to :user_id
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
