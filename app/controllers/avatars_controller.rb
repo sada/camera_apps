8b6bd9b841cd182c27b0578ab3409a6987273690
Akihiro Sada <sada@netlab.jp>
2017-04-28 20:35:58 +0900

    add create action to avatars_controller.

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,2 +1,23 @@
 class AvatarsController < ApplicationController
+  # POST /avatars
+  # POST /avatars.json
+  def create
+    @avatar = Avatar.new(avatar_params)
+
+    respond_to do |format|
+      if @avatar.save
+        format.html { redirect_to @avatar, notice: 'Avatar was successfully created.' }
+        format.json { render :show, status: :created, location: @avatar }
+      else
+        format.html { render :new }
+        format.json { render json: @avatar.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def avatar_params
+      params.require(:avatar).permit(:image)
+    end
 end
