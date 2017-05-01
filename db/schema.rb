761c1eeb6dd4510874869081a0209da2196b9af9
Akihiro Sada <sada@netlab.jp>
2017-05-01 13:46:01 +0900

    run "bundle exec rails g migration add_uuid_to_users uuid:string".

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501044257) do
+ActiveRecord::Schema.define(version: 20170501044542) do
 
   create_table "avatars", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170501044257) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "uuid"
   end
 
 end
