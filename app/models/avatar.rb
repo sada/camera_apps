67660fc120022446b749c3b38bcab44416dd2f2a
Akihiro Sada <sada@netlab.jp>
2017-05-08 19:09:21 +0900

    fix user relationship.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,5 +1,5 @@
 class Avatar < ApplicationRecord
-  belongs_to :user_id, optional: true
+  belongs_to :user, optional: true
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
